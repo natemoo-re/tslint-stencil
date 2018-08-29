e2e87c0c31ac93aac7d65f0508eabe00f75e7d76
natemoo-re <ntmoore@knox.edu>
2018-08-29 08:31:25 -0500

    fix() await for each command in publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -4,22 +4,29 @@ const exec = promisify(require('child_process').exec);
 const cmdArguments = process.argv.slice(2);
 const semverType = cmdArguments[0];
 
-if (!semverType || !semverType.match(/^(major | minor | patch | premajor | preminor | prepatch | prerelease)$/)) {
-    console.log(`Usage: major|minor|patch`);
+console.log(semverType);
+
+if (!semverType || !semverType.match(/^(major|minor|patch|premajor|preminor|prepatch|prerelease)$/)) {
+    console.log(`Usage: major|minor|patch|premajor|preminor|prepatch|prerelease`);
     process.exit(1);
 }
 
-run(`npm run verify`);
-run(`npm version ${semverType}`);
-run(`git push`);
-run(`git push --tags`);
-run(`npm publish`);
 
 async function run(command) {
     const result = await exec(command);
-
+    
     if (!!result.code) {
         console.log(result.stdout);
         process.exit(1);
     }
 }
+
+async function main() {
+    await run(`npm run verify`);
+    await run(`npm version ${semverType}`);
+    await run(`git push`);
+    await run(`git push --tags`);
+    await run(`npm publish`);
+}
+
+main();
