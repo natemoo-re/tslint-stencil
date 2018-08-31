36651c51a2a781712ca97f61cf73e35330776220
natemoo-re <ntmoore@knox.edu>
2018-08-30 19:11:42 -0500

    fix(): remove console.logs

--- src/methodDecoratorStyleRule.ts
+++ src/methodDecoratorStyleRule.ts
@@ -16,13 +16,12 @@ class MethodDecoratorWalker extends Lint.RuleWalker {
     public visitMethodDeclaration(node: ts.MethodDeclaration) {
         const [style]: ['single-line' | 'multi-line'] = this.getOptions();
         
-        console.dir({ style });
         if (style && isComponentClass(node.parent) && node.decorators && Array.isArray(node.decorators)) {
-            console.dir(node.name);
             const dec: ts.Decorator = node.decorators[node.decorators.length - 1];
             
             const { line: decoratorLine } = this.getLineAndCharacterOfPosition(dec.getEnd());
             const { line: propertyLine } = this.getLineAndCharacterOfPosition(node.name.getEnd());
+            
             if (style === 'single-line') {
                 if (decoratorLine !== propertyLine) return this.addFailure(this.createFailure(node.getStart(this.getSourceFile()), node.getWidth(), Rule.SINGLE_FAILURE_STRING));
             } else if (style === 'multi-line') {
